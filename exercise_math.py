3ea4db9962447f25a755cdbf72f8b1cd74745740
Franco-009 <fdomingueztupa@mail.austral.edu.ar>
2025-08-16 15:49:37 -0300

    Update exercise_math.py
    Franco Dominguez Tupa
    TP1

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,23 @@
-def math():
-    a = 57
-    b = 7
+a = 57
+b = 7
+suma = a + b 
+print(suma)
+
+diferencia = a - b
+print(diferencia)
+
+producto = a * b
+print(producto)
+
+promedio = (a + b) / 2                    #hay que sumarlo y despues dividirlo por 2
+print(promedio)
+
+cociente_entero = a // b                  #divicion entera
+print(cociente_entero)
+
+resto = a % b                             #modulo, lo que sobra de la divicion entera 
+print(resto) 
+
+divicion_real = a / b
+print(divicion_real)
+    
