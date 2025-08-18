89a7b610e398c6085e67ed913ed2ea1776138180
Franco-009 <fdomingueztupa@mail.austral.edu.ar>
2025-08-18 20:47:35 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,23 +1,3 @@
-a = 57
-b = 7
-suma = a + b 
-print(suma)
-
-diferencia = a - b
-print(diferencia)
-
-producto = a * b
-print(producto)
-
-promedio = (a + b) / 2                    #hay que sumarlo y despues dividirlo por 2
-print(promedio)
-
-cociente_entero = a // b                  #divicion entera
-print(cociente_entero)
-
-resto = a % b                             #modulo, lo que sobra de la divicion entera 
-print(resto) 
-
-divicion_real = a / b
-print(divicion_real)
-    
+def math():
+    a = 57
+    b = 7
